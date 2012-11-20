059766ffd40d4d7227eea2d8a571c5436f082939
Dries Desmet <dries@urga.be>
2012-11-20 17:26:54 +0100

    Improve fabric script

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,54 @@
 from __future__ import with_statement
 import os
 from fabric.api import *
+from fabric.contrib import django
 
-REPO = "git@bitbucket.org:dries/{{ project_name }}"
+django.project('{{ project_name }}')
+from django.conf import settings
 
-def bootstrap():
-    run("mkvirtualenv {{ project_name }}")
-    run("pip install -E {{ project_name }} -r requirements.txt")
+env.projectname = settings.PROJECTNAME
+env.repo = "git@bitbucket.org:dries/%(projectname)s" % env
 
 def production():
     """ Use production server settings """
     
     env.hosts=['urga@ssh.alwaysdata.com:22']
-    env['projectdir'] = "~/src/{{ project_name }}"
+    env['projectdir'] = "~/src/%(projectname)s-prod" % env
     env['branch'] = 'master'
+    env['venv'] = env.projectname
     
 def staging():
     """ Use staging server settings """
     
     env.hosts=['urga@ssh.alwaysdata.com:22']
-    env['projectdir'] = "~/src/{{ project_name }}-stage"
+    env['projectdir'] = "~/src/%(projectname)s-stage" % env
     env['branch'] = 'develop'
+    env['venv'] = "%(projectname)s-stage" % env
+
+def bootstrap():
+    run("mkvirtualenv {{ project_name }}")
+    run("pip install -E {{ project_name }} -r requirements.txt")
+
+def compileless():
+    """Compile less locally to be saved in the repo"""
+    local("lessc -x %(projectname)s/static/less/style.less > %(projectname)s/static/css/style.css" % env)
+
+def copyfixturesmedia():
+    local("cp -r %(projectname)s/fixtures/media/* public/media/" % env
+
+def savemediatofixtures():
+    """Copy current user content to fixtures."""
+    local("cp -r %s %(projectname)s/fixtures/" % (settings.MEDIA_ROOT, env)
 
 def deploy():
     """Delete and initialize deployment on the target server"""
-    #run("rm -rf %s" % env['projectdir'])
-    run("git clone -b %s %s %s" % (env.branch, REPO, env.projectdir) )
+    run("git clone -b %(branch)s %(repo)s %(projectdir)s" % env )
 
 def update():
+    compileless()
+    local('git commit -a')
+    local('git push')
     with cd(env['projectdir']):
         run('git pull')
+    with prefix('workon %s' % env.venv):
+        run('./manage.py collectstatic --noinput')

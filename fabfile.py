1fb32ecfa07a4f87809e4ffab4087a12027d6efe
Dries Desmet <dries@air-3.local>
2013-06-12 12:14:36 +0200

    Improve boilerplate

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,18 @@
 from __future__ import with_statement
 import os
-from os.path import abspath, basename, dirname
-from fabric.api import env, local, run, require, prompt, cd, prefix
+from os.path import abspath, basename, dirname, join
+from fabric.api import env, local, run, require, prompt, cd, prefix, execute, settings
+from fabric.colors import yellow as _yellow
 from fabric.contrib import django
 import site
 from django.utils.crypto import get_random_string
+import inspect
+from datetime import datetime
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+
 
 # GLOBALS
 #########
@@ -13,149 +21,202 @@ env['projectname'] = basename(dirname(abspath(__file__)))
 env["repo"] = "git@bitbucket.org:dries/%(projectname)s" % env
 env["less"] = True
 env["db_engine"] = "postgres" # Use 'postgres', 'mysql' or 'sqlite'
-env["user"] = "urga"
 
+
+def _set_alwaysdata_env():
+    env['run'] = run
+    env['accountname'] = "urga"
+    env['hosts']=['%(accountname)s@ssh.alwaysdata.com' % env, ]
+    env['venv'] = "%(projectname)s" % env
+    env['requirementsfile'] = "requirements.txt"
+
+    env['homedir']=join("/home", "%(accountname)s" % env )
+    env['basedir']= join(env.homedir, "www")
+    env['projectdir'] = join(env.basedir, env.projectname)
+    env['staticdir'] = join(env.basedir, env.projectname+"-static")
+
+    env['db_user'] = "%(accountname)s_%(projectname)s" % env
+    env["db_host"] = "adpostgresql.urga.be"
+    env['db_name'] = "%(accountname)s_%(projectname)s" % env
 
 # ENVIRONMENTS
 ##############
 
 def localhost():
-    """ Use development server settings """
-
-    env['suffix'] = "dev"
-    env['projectdir'] = dirname(abspath( __file__ ))
+    """
+    Use development server settings
+    """
+    env['settings'] = "dev"
     env['run'] = local
     env['venv'] = "%(projectname)s" % env
-    env['requirementsfile'] = "requirements_%(suffix)s.txt" % env
+    env['requirementsfile'] = "requirements_%(settings)s.txt" % env
+    
+    env['projectdir'] = dirname(abspath( __file__ ))
+    
     env['db_user'] = "%(projectname)s" % env
     env["db_host"] = "localhost"
     env['db_name'] = env.db_user
-    django.settings_module('%(projectname)s.settings.%(suffix)s' % env )
+    
 
 def staging():
-    """ Use staging server settings """
-    
-    env['suffix'] = "staging"
-    env['run'] = run
-    env['hosts']=['ssh.alwaysdata.com', ]
-    env['basedir']= os.path.join(os.environ['HOME'], "www-%(suffix)" % env)
-    env['projectdir'] = os.path.join(env.basedir, env.projectname)
-    env['mediadir'] = os.path.join(env.basedir, env.projectname+"-media")
+    """
+    Use staging server settings
+    """
+    env['settings'] = "staging"
+    env['projectname'] = "%(projectname)s-%(settings)s" % env
     env['branch'] = 'develop'
-    env['venv'] = "%(projectname)s-%(suffix)s" % env
-    env['requirementsfile'] = "requirements.txt"
-    env['db_user'] = "%(remote_username)s_%(projectname)s" % env
-    env["db_host"] = "adpostgresql.urga.be"
-    env['db_name'] = "%(remote_username)s_%(projectname)s-%(suffix)s" % env
-    django.settings_module("%(projectname)s.settings.%(suffix)s" % env)
+    _set_alwaysdata_env()
 
 def production():
-    """ Use production server settings """
-    
-    env['suffix'] = "prod"
-    env['hosts']=['ssh.alwaysdata.com', ]
-    env['basedir']= os.path.join(os.environ['HOME'], "www")
-    env['projectdir'] = os.path.join(env.basedir, env.projectname)
-    env['mediadir'] = os.path.join(env.basedir, env.projectname+"-media")
+    """
+    Use production server settings
+    """
+    env['settings'] = "prod"
     env['branch'] = 'master'
-    env['venv'] = "%(projectname)s" % env
-    env['requirementsfile'] = "requirements.txt"
-    env['db_user'] = "%(remote_username)s_%(projectname)s" % env
-    env["db_host"] = "adpostgresql.urga.be"
-    env['db_name'] = "%(remote_username)s_%(projectname)s-%(suffix)s" % env
-    django.settings_module("%(projectname)s.settings.%(suffix)s" % env)
+    _set_alwaysdata_env()
 
 # HELPERS
 #########
 def _generate_key():
-    """Generate a random key just like django-startproject does it."""
-
-    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*-_=+' #Don't allow '()' in the key to avoid problems with setting environment
+    """
+    Generate a random key just like django-startproject does it.
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*-_=+()'
     return get_random_string(50, chars)
 
 
 def _generate_db_url():
     
-    print "DATABASE SETTINGS for: %s" % env.suffix
+    print "Setting up Database for %s" % env.settings
     print "Engine: %s" % env.db_engine
     print "User: %s" % env.db_user
     print "Hostname: %s" % env.db_host
     print "Database name: %s" % env.db_name
 
-    db_password = prompt('DB Password: ', default=env.projectname)
-    
+    if not env.settings=="dev":
+        db_password = prompt(_yellow('DB Password: '))
+    else:
+        db_password = env.projectname
+
     return "%s://%s:%s@%s/%s" % (env.db_engine, env.db_user, db_password, env.db_host, env.db_name)
+
+def virtualenv(command):
+    """
+    Run command in virtualenv.
+    """
+    with prefix('source virtualenvwrapper.sh && workon %(venv)s' % env):
+        return env.run(command)
+
+def _fn():
+    """
+    Returns current function name
+    """
+    return inspect.stack()[1][3]
+
+def _db_getdict():
+    if not env.settings=="dev":
+        db_url = virtualenv("echo $DATABASE_URL")
+    else:
+        db_url = os.environ['DATABASE_URL']
+    parsed_url = urlparse.urlparse(db_url)
+
+    db_dict = {
+        'PASSWORD' : parsed_url.password,
+        'HOST' : parsed_url.hostname,
+        'USER' : parsed_url.username,
+        'NAME' : parsed_url.path[1:],
+    }
+    return db_dict
+def _db_exists():
+    d = _db_getdict()
+    shellreturn = local("PGPASSWORD=%(PASSWORD)s psql -l -h %(HOST)s -U %(USER)s| grep %(NAME)s | wc -l" % d, capture=True)
+    return int(shellreturn)
 
 # COMMANDS
 ##########
 
 def bootstrap():
-    require("venv", provided_by=[localhost, staging, production])
+    """
+    Initialize a virtual environment and configure variables
+    """
+    require("settings", provided_by=[localhost, staging, production])
     with cd("%(projectdir)s" % env):
-        # For this to work, virtualenvwrapper.sh needs to be on the system path
         env.run("source virtualenvwrapper.sh && mkvirtualenv %(venv)s && setvirtualenvproject" % env)
-    with prefix("source virtualenvwrapper.sh && workon %(venv)s" % env):
-        env.run("pip install -r %(requirementsfile)s" % env)
-        env.run("echo 'export DJANGO_SETTINGS_MODULE=%(projectname)s.settings.%(suffix)s'>>$WORKON_HOME/%(venv)s/bin/postactivate" % env)
-        env.run("echo 'unset DJANGO_SETTINGS_MODULE'>>$WORKON_HOME/%(venv)s/bin/postdeactivate" % env)
-        env.run("echo 'export DJANGO_SECRET_KEY=%s'>>$WORKON_HOME/%s/bin/postactivate" % (_generate_key(), env.venv))
-        env.run("echo 'unset DJANGO_SECRET_KEY'>>$WORKON_HOME/%(venv)s/bin/postdeactivate " % env)
-        env.run("echo 'export DATABASE_URL=%s'>>$WORKON_HOME/%s/bin/postactivate" % (_generate_db_url(), env.venv))
-        env.run("echo 'unset DATABASE_URL'>>$WORKON_HOME/%(venv)s/bin/postdeactivate " % env)
-        env.run("chmod +x ./manage.py")
+    virtualenv("pip install -r %(requirementsfile)s" % env)
+    virtualenv("echo 'export DJANGO_SETTINGS_MODULE=%(projectname)s.settings.%(settings)s'>>$WORKON_HOME/%(venv)s/bin/postactivate" % env)
+    virtualenv("echo 'unset DJANGO_SETTINGS_MODULE'>>$WORKON_HOME/%(venv)s/bin/postdeactivate" % env)
+    virtualenv("""echo "export DJANGO_SECRET_KEY='%s'">>$WORKON_HOME/%s/bin/postactivate""" % (_generate_key(), env.venv))
+    virtualenv("echo 'unset DJANGO_SECRET_KEY'>>$WORKON_HOME/%(venv)s/bin/postdeactivate " % env)
+    virtualenv("""echo "export DATABASE_URL='%s'">>$WORKON_HOME/%s/bin/postactivate""" % (_generate_db_url(), env.venv))
+    virtualenv("echo 'unset DATABASE_URL'>>$WORKON_HOME/%(venv)s/bin/postdeactivate" % env)
+    virtualenv("chmod +x ./manage.py")
         
 
+def update_requirements():
+    with prefix("source virtualenvwrapper.sh && workon %(venv)s" % env):
+        env.run('pip install -r requirements.txt --upgrade')
+
 def update(skipreq=True):
-    "Update the remote target. By default it skips checking requirements. Use 'update:skipreq=False' to force updating requirements."
+    """
+    Update the remote target. By default it skips checking requirements. Use 'update:skipreq=False' to force updating requirements.
+    """
 
     with prefix('workon %(venv)s' % env):
-        run('git pull')
+        env.run('git pull')
         if env.less:
             run('lessc -x %(projectname)s/static/less/style.less > %(projectname)s/static/css/style.css' % env)
         if skipreq in ("False", "false"): # Have to test against a string, because the skipreq parameter is not a boolean, but a string.
-            run('pip install -r requirements.txt --upgrade')
-        run('./manage.py collectstatic --noinput --settings=%(settings_module)s' % env)
-        run('cp apache/.htaccess public/')
-        run('cp apache/django_%(suffix)s.fcgi public/django.fcgi' % env)
+            execute(update_requirements)
+        env.run('./manage.py collectstatic --noinput --no-default-ignore')
+        env.run('cp fcgi/django_%(settings)s.fcgi fcgi/django.fcgi' % env)
 
 def deploy():
-    """Delete and initialize deployment on a remote server"""
-
-    run("rm -rf %(projectdir)s" % env)
-    run("git clone -b %(branch)s %(repo)s %(projectdir)s" % env )
-    bootstrap()
+    """
+    Delete and initialize deployment on a remote server
+    """
+    require("settings", provided_by=[staging, production])
+    env.run("rm -rf %(projectdir)s" % env)
+    env.run("git clone -b %(branch)s %(repo)s %(projectdir)s" % env )
+    execute(bootstrap)
     update(skipreq=False)
 
-def dumpdata():
-    "Dump sql to local file named dumpdata.sql using pg_dump."
 
-    site.addsitedir(abspath(os.path.dirname(__file__)))
-    from django.conf import settings
-    local("PGPASSWORD=%s pg_dump -O -x -h %s -U %s %s > dumpdata.sql" % (
-        settings.DATABASES['default']['PASSWORD'],
-        settings.DATABASES['default']['HOST'],
-        settings.DATABASES['default']['USER'],
-        settings.DATABASES['default']['NAME'],
-        )
-    )
+# Database operations
+#####################
 
-def restoredata():
-    site.addsitedir(abspath(os.path.dirname(__file__)))
-    from django.conf import settings
-    local("PGPASSWORD=%s psql -1 -h %s -U %s %s < dumpdata.sql" % (
-        settings.DATABASES['default']['PASSWORD'],
-        settings.DATABASES['default']['HOST'],
-        settings.DATABASES['default']['USER'],
-        settings.DATABASES['default']['NAME'],
-        )
-    )
+def db_dump(dumpfile="sql/dumpdata.sql"):
+    print(_yellow('>>> starting %s()' % _fn()))
+    require("settings", provided_by=[localhost, staging, production])
+    context =  _db_getdict()
+    context['DUMPFILE'] = dumpfile
+    local("PGPASSWORD=%(PASSWORD)s pg_dump -O -x -h %(HOST)s -U %(USER)s %(NAME)s>%(DUMPFILE)s" % context )
+
+def db_drop():
+    print(_yellow('>>> starting %s()' % _fn()))
+    local("PGPASSWORD=%(PASSWORD)s dropdb -h %(HOST)s -U %(USER)s %(NAME)s" % _db_getdict())
+
+def db_create():
+    print(_yellow('>>> starting %s()' % _fn()))
+    local("PGPASSWORD=%(PASSWORD)s createdb -h %(HOST)s -O %(USER)s %(NAME)s " % _db_getdict())
+
+def db_restore(dumpfile="sql/dumpdata.sql"):
+    print(_yellow('>>> starting %s()' % _fn()))
+    require("settings", provided_by=[localhost, staging, production])
+    if _db_exists():
+        filename = "sql/%s" % env.settings + datetime.now().strftime('_%H_%M_%d_%m_%Y.sql')
+        db_dump(dumpfile=filename)
+        with settings(warn_only=True):
+            db_drop()
+    with settings(warn_only=True):
+        db_create()
+    context = _db_getdict()
+    context['DUMPFILE'] = dumpfile
+    local("PGPASSWORD=%(PASSWORD)s psql -1 -h %(HOST)s -U %(USER)s %(NAME)s < %(DUMPFILE)s" % context )
 
 def getmedia():
-    local('rm -r public/media')
-    local('mkdir public/media')
-    get('%(mediadir)s/*' % env, 'public/media/')
+    require("settings", provided_by=["staging", "production"])
+    local('rsync -r -L --delete -vv %(host_string)s:%(staticdir)s/media/ public/media' % env)
 
 def putmedia():
-    run('mkdir -p %(mediadir)s' % env)
-    run('ln -snf %(mediadir)s %(projectdir)s/public/media' % env)
-    put('public/media/*', '%(mediadir)s' % env)
+    require("settings", provided_by=["staging", "production"])
+    local('rsync -r -L --delete -vv public/media/ %(host_string)s:%(staticdir)s/media/' % env)
--- project_name/settings/prod.py
+++ project_name/settings/prod.py
@@ -1,6 +1,24 @@
 from .common import *
+from os.path import normpath, dirname, join, basename
 import dj_database_url
+
 
 DATABASES['default'] =  dj_database_url.config()
 
 ALLOWED_HOSTS = ['www.{{ project_name }}.be']
+
+########## STATIC FILE CONFIGURATION
+STATIC_ROOT = normpath(join(dirname(SITE_ROOT), "{{ project_name }}-static"))
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL = 'http://static.{{ project_name }}.be/'
+########## END STATIC FILE CONFIGURATION
+
+########## MEDIA CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+MEDIA_ROOT = normpath(join(dirname(SITE_ROOT), basename(SITE_ROOT)+"-static", "meda"))
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = STATIC_URL + 'media/'
+########## END MEDIA CONFIGURATION
+
+ADMIN_MEDIA_PREFIX = 'http://static.{{ project_name }}.be/admin/'
